a94543373d4c497b497577360c091dba0e0539e5
Aprendiz <samuelsolanoama@gmail.com>
2023-07-11 11:55:20 -0500

    k

--- app/src/main/java/com/example/databaseroom/DataBase/Sexo.kt
+++ app/src/main/java/com/example/databaseroom/DataBase/Sexo.kt
@@ -2,9 +2,10 @@ package com.example.databaseroom.DataBase
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "Sexo")
+@Entity(tableName = "Sexo", indices = [Index(value = ["tipo"], unique = true)])
 data class Sexo(
     @PrimaryKey(autoGenerate = true)
     var id: Int,

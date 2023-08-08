6691ea859f1c88cdd35213f967bbe62ab2140507
jean perez <perezepiayujeancarlos@gmail.com>
2023-08-08 09:16:32 -0500

    a

--- app/src/main/java/com/example/databaseroom/DataBase/Dao/ISexo.kt
+++ app/src/main/java/com/example/databaseroom/DataBase/Dao/ISexo.kt
@@ -1,6 +1,7 @@
 package com.example.databaseroom.DataBase.Dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy.Companion.IGNORE
 import androidx.room.Query
@@ -12,4 +13,6 @@ interface ISexo {
     suspend fun Insertar(sexoEntity: SexoEntity)
     @Query("SELECT * FROM SexoEntity")
     fun Get():List<SexoEntity>
+    @Delete
+    fun Eliminar(sexoEntity: SexoEntity)
 }
--- app/src/main/java/com/example/databaseroom/DataBase/Entitys/SexoEntity.kt
+++ app/src/main/java/com/example/databaseroom/DataBase/Entitys/SexoEntity.kt
@@ -2,12 +2,14 @@ package com.example.databaseroom.DataBase.Entitys
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(indices = [Index(value = ["nombre"],
+    unique = true)])
 data class SexoEntity(
     @PrimaryKey(autoGenerate = true)
     var id:Int,
-    @ColumnInfo
+    @ColumnInfo(name ="nombre")
     var nombre:String
 )
